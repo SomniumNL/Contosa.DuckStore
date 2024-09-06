4ce244c8cb4976a05db16dda4041cb93d7172206
Somnium <michbakker@hotmail.com>
2024-09-06 04:47:36 +0200

    Update

---
+++ Source/Contosa.DuckStore.PublicApi.Business/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Contosa.DuckStore.PublicApi.Business
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Source/Contosa.DuckStore.PublicApi.DataAccess/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Contosa.DuckStore.PublicApi.DataAccess
+{
+    public class Class1
+    {
+
+    }
+}
