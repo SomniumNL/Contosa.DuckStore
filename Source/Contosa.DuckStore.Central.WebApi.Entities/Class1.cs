8f2931cf7d7d59b681106acf7dd6b62f38408741
Somnium <michbakker@hotmail.com>
2024-11-16 02:37:10 +0100

    Update

---
+++ Source/Contosa.DuckStore.Central.WebApi.Entities/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Contosa.DuckStore.Central.WebApi.Entities
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Source/Contosa.DuckStore.Management.WebApp.Entities/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Contosa.DuckStore.Management.WebApp.Entities
+{
+    public class Class1
+    {
+
+    }
+}
