cc2e159a1e7176ac92b5b7a45ef22c65abfb64c3
Somnium <michbakker@hotmail.com>
2024-11-17 19:05:52 +0100

    Update structure

--- Test Area/TestAreaConsole/Program.cs
+++ Test Area/TestAreaConsole/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace TestAreaConsole
+﻿namespace TestAreaConsole;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
